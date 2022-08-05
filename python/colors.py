4fe0bf78d5c6aebb589e6bacaee343960be236c6
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-08-05 14:59:24 -0500

    Use printError() when required RGB color files do not exist.

--- python/colors.py
+++ python/colors.py
@@ -10,29 +10,43 @@ import ROOT
 # download text file using "wget https://xkcd.com/color/rgb.txt"
 # generate json file from the text file by running main()
 
+def printError(file_name):
+    print("ERROR: The required file \"{0}\" does not exist.".format(file_name))
+    print(" - First, download rgb.txt using \"wget https://xkcd.com/color/rgb.txt\".")
+    print(" - Then, run the script \"python python/colors.py\" to produce \"rbg.json\".")
+
 # print color, RGB, and index
 def printColors():
     json_file = "rgb.json"
-    with open(json_file, "r") as input_file:
-        data = json.load(input_file)
-        for color in data:
-            index = ROOT.TColor.GetColor(data[color])
-            print("{0}: {1}: {2}".format(color, data[color], index))
+    # check that json file exists
+    if not os.path.isfile(json_file):
+        printError(json_file)
+    else:
+        with open(json_file, "r") as input_file:
+            data = json.load(input_file)
+            for color in data:
+                index = ROOT.TColor.GetColor(data[color])
+                print("{0}: {1}: {2}".format(color, data[color], index))
 
 # return TColor index given xkcd color using RGB json file 
 def getColorIndex(color):
+    index = -1
     json_file = "rgb.json"
-    with open(json_file, "r") as input_file:
-        data = json.load(input_file)
-        # if color in map, get RGB from map
-        if color in data:
-            index = ROOT.TColor.GetColor(data[color])
-            #print("{0}: {1}: {2}".format(color, data[color], index)) 
-        # otherwise, assume RBG are provided
-        else:
-            print("WARNING: The color '{0}' is not found in the map loaded from {1}. Assuming '{0}' is an RGB color and passing to ROOT.TColor.GetColor(color).".format(color, json_file))
-            index = ROOT.TColor.GetColor(color)
-        return index
+    # check that json file exists
+    if not os.path.isfile(json_file):
+        printError(json_file)
+    else:
+        with open(json_file, "r") as input_file:
+            data = json.load(input_file)
+            # if color in map, get RGB from map
+            if color in data:
+                index = ROOT.TColor.GetColor(data[color])
+                #print("{0}: {1}: {2}".format(color, data[color], index)) 
+            # otherwise, assume RBG are provided
+            else:
+                print("WARNING: The color '{0}' is not found in the map loaded from {1}. Assuming '{0}' is an RGB color and passing to ROOT.TColor.GetColor(color).".format(color, json_file))
+                index = ROOT.TColor.GetColor(color)
+    return index
 
 # make RGB json file from xkcd colors text file
 def main():
@@ -42,9 +56,7 @@ def main():
     
     # check that input file exists
     if not os.path.isfile(input_name):
-        print("ERROR: The required input file \"{0}\" does not exist.".format(input_name))
-        print(" - First, download this required text file using \"wget https://xkcd.com/color/rgb.txt\"")
-        print(" - Then run this script again to produce \"{0}\"".format(output_name))
+        printError(input_name)
         return
     
     with open(input_name, "r") as input_file:
@@ -67,7 +79,9 @@ if __name__ == "__main__":
     main()
     
     # example of getting color index
-    #getColorIndex("tomato")
+    #color = "tomato"
+    #index = getColorIndex(color)
+    #print("color: {0}, index: {1}".format(color, index))
     
     # print colors
     #printColors()
